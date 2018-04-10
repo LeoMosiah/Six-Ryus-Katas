60d79254ccb734b0e8b1b9a09e0b12c84b21ed15
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-04-10 14:42:41 -0300

    Update conversorDeRomanos.js

--- conversorDeRomanos.js
+++ conversorDeRomanos.js
@@ -1,7 +1,7 @@
 const dicionario = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 }
 const isValidRoman = /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{}0,3})(IX|IV|V?I{0,3})$/;
 function converterRomanos(emRomanos){ 
-  if(isValidRoman.test(string)===true)
+  if(isValidRoman.test(emRomanos)===true)
     return "Não é válido" ;
   const emDecimal = emRomanos.split('').map((elemento)=>dicionario[elemento]);
   for(let i=0;i<emDecimal.length;i++){
