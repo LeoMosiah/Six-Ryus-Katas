5312e2173d781e5266eb17fb834c6815381e0500
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-21 14:01:25 -0300

    Create responderPergunta.js

---
+++ responderPergunta.js
@@ -0,0 +1,15 @@
+function responderPergunta(pergunta) {
+    const resposta = [];
+    const howManyQuestion = /how many/ig;
+    const howMuchQuestion = /how much/ig;
+    for(let i= 0;i<pergunta.length;i++){
+    if(!howMuchQuestion.test(pergunta[i]) && !howMuchQuestion.test(pergunta[i])){
+        resposta.push("I have no idea what you are talking about")
+        } else if (howManyQuestion.test(pergunta[i]){
+            resposta.push(`${pergunta[i].slice(pergunta[i].indexOf('is')+1,pergunta.length - 2).join('')}
+             ${(converterRomanos(converterAlien(pergunta)))}`)
+        } else{
+            resposta.push()
+        }
+    }
+}
