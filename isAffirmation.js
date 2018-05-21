c73efa66d4c3e5a836f0bae1ad45e1d9c38546d9
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-21 10:30:33 -0300

    Update isAffirmation.js

--- isAffirmation.js
+++ isAffirmation.js
@@ -1,6 +1,6 @@
 function isAffirmation(dadosOriginais){
-	let affirmations = [];
-	for (int i = 0;i<dadosOriginais.length;i++){
+	const affirmations = [];
+	for (let i = 0;i<dadosOriginais.length;i++){
 		if(dadosOriginais[i].indexOf('?') === -1){
 			affirmations.push(dadosOriginais[i].trim()) 
 		}
