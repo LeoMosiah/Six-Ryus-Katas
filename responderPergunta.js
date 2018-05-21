8144f5d716bff532ec64be08a7bdc3b2ebb3bcec
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-21 15:47:13 -0300

    Update responderPergunta.js

--- responderPergunta.js
+++ responderPergunta.js
@@ -1,3 +1,4 @@
+// importar conversorMonetario
 function responderPergunta(pergunta) {
     const resposta = [];
     const howManyQuestion = /how many/ig;
@@ -5,11 +6,13 @@ function responderPergunta(pergunta) {
     for(let i= 0;i<pergunta.length;i++){
     if(!howMuchQuestion.test(pergunta[i]) && !howMuchQuestion.test(pergunta[i])){
         resposta.push("I have no idea what you are talking about")
-        } else if (howManyQuestion.test(pergunta[i]){
+        } else if (howManyQuestion.test(pergunta[i])){
             resposta.push(`${pergunta[i].slice(pergunta[i].indexOf('is')+1,pergunta.length - 2).join('')}
              ${(converterRomanos(converterAlien(pergunta)))}`)
         } else{
-            resposta.push()
+            resposta.push(`${pergunta[i].slice(pergunta[i].indexOf('is')+1,pergunta.length - 3).join('')}
+             ${(converterRomanos(converterAlien(pergunta)))}`)
         }
     }
+    return resposta.join('\n');
 }
