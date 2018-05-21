2ca429e5f92294efc63391e0a9dd60fc7bd5fcdc
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-21 10:20:06 -0300

    Update isQuestion.js

--- isQuestion.js
+++ isQuestion.js
@@ -1,12 +1,9 @@
 function isQuestion(dadosOriginais){
 	let questions = [];
-	let affirmations = [];
 	for (int i = 0;i<dadosOriginais.length;i++){
 		if(dadosOriginais[i].toString().indexOf('?') > -1){
-			questions.push(dadosOriginais[i].toString()) 
-		} else {
-			affirmations.push(dadosOriginais[i].toString())
+			questions.push(dadosOriginais[i].trim()) 
 		}
 	}
-	return [questions,affirmations]
+	return questions
 }
