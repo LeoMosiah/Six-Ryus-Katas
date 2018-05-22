474055181c81a0b24a01dc08ea701a651862903a
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-22 13:55:18 -0300

    Delete isAffirmation.js

--- isAffirmation.js
+++
@@ -1,9 +0,0 @@
-function isAffirmation(dadosOriginais){
-	const affirmations = [];
-	for (let i = 0;i<dadosOriginais.length;i++){
-		if(dadosOriginais[i].indexOf('?') === -1){
-			affirmations.push(dadosOriginais[i].trim()) 
-		}
-	}
-	return affirmations
-}
