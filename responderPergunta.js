5e7592c5fd8b8e95ae1959ed14faffb05bd110a8
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-22 12:51:24 -0300

    Update responderPergunta.js

--- responderPergunta.js
+++ responderPergunta.js
@@ -1,18 +1,34 @@
 // importar conversorMonetario
 function responderPergunta(pergunta) {
-    const resposta = [];
-    const howManyQuestion = /how many/ig;
-    const howMuchQuestion = /how much/ig;
-    for(let i= 0;i<pergunta.length;i++){
-    if(!howMuchQuestion.test(pergunta[i]) && !howMuchQuestion.test(pergunta[i])){
-        resposta.push("I have no idea what you are talking about")
-        } else if (howManyQuestion.test(pergunta[i])){
-            resposta.push(`${pergunta[i].slice(pergunta[i].indexOf('is')+1,pergunta.length - 2).join('')}
-             ${(converterRomanos(converterAlien(pergunta)))}`)
-        } else{
-            resposta.push(`${pergunta[i].slice(pergunta[i].indexOf('is')+1,pergunta.length - 3).join('')}
-             ${(converterRomanos(converterAlien(pergunta)))}`)
-        }
+  const resposta = [];
+  const howManyQuestion = /how many/gi;
+  const howMuchQuestion = /how much/gi;
+  for (let i = 0; i < pergunta.length; i++) {
+    let auxiliar = pergunta[i].split(" ");
+    if (pergunta[i]) {
+      resposta.push("I have no idea what you are talking about");
+    } else if (howMuchQuestion.test(pergunta[i])) {
+      resposta.push(`${auxiliar
+        .slice(auxiliar.indexOf("is") + 1, auxiliar.length - 1)
+        .join(" ")}
+      ${deRomanoParaDecimal(
+        deAlienParaRomano(
+          auxiliar.slice(auxiliar.indexOf("is") + 1, auxiliar.length - 1)
+        )
+      )}`);
+    } else {
+      resposta.push(`${auxiliar
+        .slice(auxiliar.indexOf("is") + 1, auxiliar.length - 1)
+        .join(" ")}
+    ${toCredits(
+      auxiliar.length - 1,
+      deRomanoParaDecimal(
+        deAlienParaRomano(
+          auxiliar.slice(auxiliar.indexOf("is") + 1, auxiliar.length - 1)
+        )
+      )
+    )}
+    `);
     }
-    return resposta.join('\n');
-}
+  }
+  return resposta.join("\n");
