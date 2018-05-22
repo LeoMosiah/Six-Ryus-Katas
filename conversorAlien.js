f4fc4277574739cdfdfedf143a65dfe63127aab8
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-22 13:54:37 -0300

    Delete conversorAlien.js

--- conversorAlien.js
+++
@@ -1,4 +0,0 @@
-const mapa = { 'glob':'I', 'prok':'V', 'pish':'X', 'tegj':'L'}
-function converterAlien(emAlien){
-return emAlien.split(' ').map((elemento)=>mapa[elemento]).join().replace(/,/ig,'');
-}
