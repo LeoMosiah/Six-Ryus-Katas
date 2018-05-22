5efc37df0462c4c5fc21d6f2f2235fa8d240dbce
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-22 13:55:49 -0300

    Delete isQuestion.js

--- isQuestion.js
+++
@@ -1,9 +0,0 @@
-function isQuestion(dadosOriginais){
-	const questions = [];
-	for (let i = 0;i<dadosOriginais.length;i++){
-		if(dadosOriginais[i].toString().indexOf('?') > -1){
-			questions.push(dadosOriginais[i].trim()) 
-		}
-	}
-	return questions
-}
