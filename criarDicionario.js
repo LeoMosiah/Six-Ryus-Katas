33e8224174a8c07e82f8028c3ea71253b57c15e4
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-05-22 13:55:10 -0300

    Delete criarDicionario.js

--- criarDicionario.js
+++
@@ -1,9 +0,0 @@
-function criarDicionario (afirmacoes){
- let map = new Map();	
-	for (let i = 0;i<afirmacoes.length();i++){
-		let palavra = afirmacoes[i].split(' ')
-		map.set(palavra[0],palavra[2])
-	}
-	return map;
- }
- 
